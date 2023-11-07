f683963961db46f8c0b4195a3ad6dff6939c540e
DanKos22 <G00397054@atu.ie>
2023-11-07 15:07:47 +0000

    Started project

---
+++ src/main/java/ie/atu/labexamnov2023/LabExamNov2023Application.java
@@ -0,0 +1,13 @@
+package ie.atu.labexamnov2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabExamNov2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabExamNov2023Application.class, args);
+    }
+
+}
